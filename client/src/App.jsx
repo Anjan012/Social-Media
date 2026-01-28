81128a5af4628fecd3f6be228ed6329aa987ef4b
Anjan012 <anjankhadka199@gmail.com>
2026-01-28 12:43:04 +0545

    define route in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { Navbar } from "./components/ui/shared/Navbar";
 import { ProfileHeader } from "./pages/Profile";
-import {SignIn} from "./pages/Auth/Signin";
-import {SignUp} from "./pages/Auth/Signup";
-import './App.css'
+import { SignIn } from "./pages/Auth/Signin";
+import { SignUp } from "./pages/Auth/Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './App.css';
+import { Home } from "./pages/Home";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path:'/signup',
+    element: <SignUp />
+  },
+  {
+    path:'/signin',
+    element: <SignIn />
+  }
+]);
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      {/* <ProfileHeader /> */}
-      <SignUp />
-      
+      <RouterProvider router={appRouter}/>
+
     </>
   )
 }
