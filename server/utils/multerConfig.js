56d71edec5f3c22448ce7ea5f257a0474be0f9a4
Anjan012 <anjankhadka199@gmail.com>
2026-02-08 11:10:36 +0545

    base multer setup logic done

---
+++ server/utils/multerConfig.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import cryto from "crypto";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const UserId = req.id;
+    const baseDir = path.join("uploads", "users", UserId.toString(), "posts");
+
+    // create dir if not exists
+    fs.mkdirSync(baseDir, {recursive: true});
+
+    cb(null, baseDir);
+  },
+  filename: function (req, file, cb) {
+    cryto.randomBytes(12, function (err, name) {
+      const fn = `post_${name.toString("hex") + path.extname(file.originalname)}`;
+      cb(null, fn);
+    });
+  }
+});
+
+
+const upload = multer({ storage: storage })
