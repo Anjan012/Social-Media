73ed250ee99d7bc0460db6cf51023c13265f5e06
Anjan012 <anjankhadka199@gmail.com>
2026-02-13 12:54:49 +0545

    update the route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/signup').post(signUp);
 router.route('/signin').post(signIn);
-router.route('/profile').get(isAuthenticated, getUserProfile);
+router.route('/profile/:id').get(isAuthenticated, getUserProfile);
 router.route('/profile/update').patch(isAuthenticated, updateProfile);
 router.route('/logout').get(logout);
 
