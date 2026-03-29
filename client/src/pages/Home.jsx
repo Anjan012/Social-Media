fe716c857d9678b193f0470c237c07f3f5d465a4
Anjan012 <anjankhadka199@gmail.com>
2026-03-29 11:05:01 +0545

    count comment in profile post and in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -252,7 +252,11 @@ export const Home = ({
                       <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                       </svg>
-                      <span>347</span>
+                      <span>
+                        {
+                          post.comments.length
+                        }
+                      </span>
                     </button>
 
                     <button className="flex items-center gap-1.5 text-gray-600 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-400 transition-colors">
