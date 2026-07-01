3cfd1dc4be964569c1e6530d699a713827f2f114
Anjan012 <anjankhadka199@gmail.com>
2026-07-01 13:57:30 +0545

    feat: Update model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -18,9 +18,23 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters long"],
+      default: null,
     },
+
+    // OAuth fields-------------------------
+    provider: {
+      type: String,
+      enum: ["local", "facebook", "google"],
+      default: "local",
+    },
+
+    providerId: {
+      type: String,
+      default: null,
+    },
+    // -------------------
+
     fullname: {
       type: String,
       trim: true,
@@ -51,6 +65,8 @@ const userSchema = new mongoose.Schema(
     dob: {
       type: Date,
     },
+    facebookId,
+    provider,
 
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
@@ -60,6 +76,10 @@ const userSchema = new mongoose.Schema(
   },
 );
 
+userSchema.index(
+  {provider: 1, providerId: 1},
+  {unique: true, sparse: true}
+);
+
+
 export const User = mongoose.model("User", userSchema);
-
-
