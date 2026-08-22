b503d7ed0153d7356c2fdd80ec4750a78f24f88d
Anjan012 <anjankhadka199@gmail.com>
2026-08-22 17:07:19 +0545

    feat: setupping the skeleton loader for profile

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -16,38 +16,51 @@ export function Profile() {
   const [profileData, setProfileData] = useState([]);
   const [user, setUser] = useState([]);
   const [posts, setPosts] = useState([]);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const PROFILE_URL = `${API_URL}/api/v1/users/${id}`;
-
     const getProfile = async () => {
-      const response = await axios.get(PROFILE_URL, { withCredentials: true });
-      // setProfileData(response.data);
-      // setUser(response.data.user);
-      // setPosts(response.data.posts);
-      // console.log(response.data);
-      setProfileData(response.data.userData);
-      setUser(response.data.userData.user);
-      setPosts(response.data.userData.posts);
+      try {
+        setLoading(true);
+
+        const PROFILE_URL = `${API_URL}/api/v1/users/${id}`;
+
+        const response = await axios.get(PROFILE_URL, {
+          withCredentials: true,
+        });
+
+        const userData = response.data.userData;
+
+        setProfileData(userData);
+        setUser(userData.user);
+        setPosts(userData.posts);
+      } catch (error) {
+        console.error("Failed to load profile:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     getProfile();
 
-  }, []);
+  }, [id]);
 
   return (
     <>
       <Navbar />
       <div className="w-full bg-white dark:bg-gray-950 border-b border-gray-200 dark:border-gray-800">
         {/* Cover area */}
-        {
-          user?.coverPicture ? (
-            <img src={user.coverPicture} alt="Cover" className="w-full h-68 object-cover" />
-          ) : (
-            <div className="h-32 sm:h-48 md:h-56 bg-linear-to-r from-red-400/20 to-purple-500/20 relative" />
-          )
-        }
+        {loading ? (
+          <div className="w-full h-68 bg-gray-200 dark:bg-gray-800 animate-pulse" />
+        ) : user?.coverPicture ? (
+          <img
+            src={user.coverPicture}
+            alt="Cover"
+            className="w-full h-68 object-cover"
+          />
+        ) : (
+          <div className="h-32 sm:h-48 md:h-56 bg-linear-to-r from-red-400/20 to-purple-500/20 relative" />
+        )}
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <ProfileSection profileData={profileData} user={user} />
           <ProfileTab />
