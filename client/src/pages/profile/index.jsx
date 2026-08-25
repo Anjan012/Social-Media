5e8f69d7473a468e046ca044b114079434b9cfeb
Anjan012 <anjankhadka199@gmail.com>
2026-08-25 20:11:22 +0530

    feat: update post box with post composer

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -9,6 +9,7 @@ import { PostBox } from "../shared/Postbox";
 import { ProfilePost } from "./profile-page/ProfilePost";
 import { ProfileSectionSkeleton } from "./skeleton/ProfileSectionSkeleton";
 import { ProfilePostSkeleton } from "./skeleton/ProfilePostSkeleton";
+import { PostComposer } from "../../components/post/PostComposer";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -74,7 +75,7 @@ export function Profile() {
           )}
 
           <ProfileTab />
-          <PostBox />
+          <PostComposer />
           {
             loading ? (
               <ProfilePostSkeleton />
